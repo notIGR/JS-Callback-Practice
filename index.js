ac47c9357150d937f7474b3070ff5698c945572b
Isaac Rodriguez <isaac.bh.rod@gmail.com>
2022-04-25 13:43:31 -0400

    setting NESW,

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
-const inventory = newInventory()
-move(inventory).to(0, 0)
+const inventory = newInventory();
+move(inventory).to(0, 0);
 
-const character = newImage('assets/green-character/static.gif')
+const character = newImage("assets/green-character/static.gif");
 let direction = null;
 let x = 100;
-let y = 250
-move(character).to(100, 250)
+let y = 250;
+if (direction === "west") {
+  x = x - 1;
+}
+if (direction === "east") {
+    x = x + 1;
+  }
+  if (direction === "south") {
+    x = x - 1;
+  }
+  if (direction === "north") {
+    x = x + 1;
+  }
+  character.style.left =x + 'px'
+  character.style.bottom = y + 'px'
+  //should these be contained inside a function???
+move(character).to(100, 250);
 
-
-move(newImage('assets/tree.png')).to(200, 450)
-move(newImage('assets/pillar.png')).to(350, 250)
-move(newImage('assets/pine-tree.png')).to(450, 350)
-move(newImage('assets/crate.png')).to(150, 350)
-move(newImage('assets/well.png')).to(500, 575)
-move(newItem('assets/sword.png')).to(500, 555)
-move(newItem('assets/shield.png')).to(165, 335)
-move(newItem('assets/staff.png')).to(600, 250)
+move(newImage("assets/tree.png")).to(200, 450);
+move(newImage("assets/pillar.png")).to(350, 250);
+move(newImage("assets/pine-tree.png")).to(450, 350);
+move(newImage("assets/crate.png")).to(150, 350);
+move(newImage("assets/well.png")).to(500, 575);
+move(newItem("assets/sword.png")).to(500, 555);
+move(newItem("assets/shield.png")).to(165, 335);
+move(newItem("assets/staff.png")).to(600, 250);
