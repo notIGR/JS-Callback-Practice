7c2b18aac107039d2e9f2ed2dfd503f0a0a4e540
Isaac Rodriguez <isaac.bh.rod@gmail.com>
2022-05-10 12:58:54 -0400

    tried moving function orders

--- index.js
+++ index.js
@@ -3,17 +3,6 @@ move(inventory).to(0, 0);
 
 const character = newImage("assets/green-character/static.gif");
 
-move(character).moveWithArrowKey(100, 250);
-
-move(newImage("assets/tree.png")).to(200, 450);
-move(newImage("assets/pillar.png")).to(350, 250);
-move(newImage("assets/pine-tree.png")).to(450, 350);
-move(newImage("assets/crate.png")).to(150, 350);
-move(newImage("assets/well.png")).to(500, 575);
-move(newItem("assets/sword.png")).to(500, 555);
-move(newItem("assets/shield.png")).to(165, 335);
-move(newItem("assets/staff.png")).to(600, 250);
-
 // alternate way to use 'setInterval'
 setInterval(() => {
   if (direction === "west") {
@@ -31,3 +20,14 @@ setInterval(() => {
   character.style.left = x + "px";
   character.style.bottom = y + "px";
 }, 1);
+
+move(character).moveWithArrowKey(100, 250);
+
+move(newImage("assets/tree.png")).to(200, 450);
+move(newImage("assets/pillar.png")).to(350, 250);
+move(newImage("assets/pine-tree.png")).to(450, 350);
+move(newImage("assets/crate.png")).to(150, 350);
+move(newImage("assets/well.png")).to(500, 575);
+move(newItem("assets/sword.png")).to(500, 555);
+move(newItem("assets/shield.png")).to(165, 335);
+move(newItem("assets/staff.png")).to(600, 250);
